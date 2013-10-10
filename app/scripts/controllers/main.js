b9ae16ddc66cdb6f4dec13a4442df06d4e9450ae
Drappier C <drappier.charles@gmail.com>
2013-10-10 15:10:45 +0200

    v2

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,39 +1,48 @@
 'use strict';
 
 angular.module('jsheroApp')
-	.controller('MainCtrl',['$scope',function ($scope) {
+	.controller('MainCtrl',['$scope','Game',function ($scope,game) {
 		$scope.start = function() {
-            this.audioElement = document.createElement('audio');
-            this.audioElement.setAttribute('src', song.url);
-            this.audioElement.setAttribute('ontimeupdate','mainController.updateCurrentDisplay(this.currentTime,song.touchkeys)');
-            this.audioElement.play();
+			$('audio').attr('src',song.url)
+            if($('.player_audio').paused != false)
+            	$('audio').get(0).play();
+            else
+            	$('audio').get(0).pause();
 		}
+
+		$("audio").bind('timeupdate', function() {
+			game.updateCurrentDisplay(this.currentTime,song.touchkeys);
+		});
+
 		$scope.score = 0;
   }]);
 
+angular.module('jsheroApp').
+	factory('Game',[function() {
 
-var mainController = {
-	updateCurrentDisplay:function(currentTime,touchs) {
-		var keepTime = 1
-		for(var i=0 ;i<touchs.length; i++) {
-			var touchkey = touchkeys[i];
-			if(touchkey.timestamp-keepTime<currentTime && touchkey.timestamp+keepTime>currentTime && !this.exist(touchkey.id)) {
-				this.addTouch(touchkey.color,touchkey.id)
-			} else if(this.exist(touchkey.id) && (touchkey.timestamp-keepTime>currentTime || touchkey.timestamp+keepTime<currentTime)) {
-				this.removeTouch(touchkey.id)
+	return {
+		updateCurrentDisplay:function(currentTime,touchs) {
+			var keepTime = 1
+			for(var i=0 ;i<touchs.length; i++) {
+				var touchkey = touchkeys[i];
+				if(touchkey.timestamp-keepTime<currentTime && touchkey.timestamp+keepTime>currentTime && !this.exist(touchkey.id)) {
+					this.addTouch(touchkey.color,touchkey.id)
+				} else if(this.exist(touchkey.id) && (touchkey.timestamp-keepTime>currentTime || touchkey.timestamp+keepTime<currentTime)) {
+					this.removeTouch(touchkey.id)
+				}
 			}
+		},
+
+		addTouch: function(color,id) {
+			$('.track[ref='+color+']').append('<div class="touchkey" ref="'+id+'"></div>');
+		},
+
+		removeTouch: function(id) {
+			$('.touchkey[ref='+id+']').remove();
+		},
+
+		exist: function(id) {
+			return ($('.touchkey[ref='+id+']').length != 0)
 		}
-	},
-
-	addTouch: function(color,id) {
-		$('.track[ref='+color+']').append('<div class="touchkey" ref="'+id+'"></div>');
-	},
-
-	removeTouch: function(id) {
-		$('.touchkey[ref='+id+']').remove();
-	},
-
-	exist: function(id) {
-		return ($('.touchkey[ref='+id+']').length != 0)
 	}
-}
+}]);
